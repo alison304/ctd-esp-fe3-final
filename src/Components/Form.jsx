a83a03c27fc003d02e3f341b76be866125955f31
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2024-09-12 21:47:20 -0500

    validations

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,15 +5,15 @@ const Form = () => {
   const [values, setValues] = React.useState({
     fullname: "",
     email: "",
-    flagMessageSuccess: false,
-    flagMessageError: false
+    flagValidation: false,
+    isSubmit: false
   });
 
   function handleChange(event) {
     const newValues = {
       ...values,
-      flagMessageSuccess: false,
-      flagMessageError: false,
+      flagValidation: false,
+      isSubmit: false,
       [event.target.name]: event.target.value
     };
     setValues(newValues);
@@ -21,14 +21,23 @@ const Form = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     let newValues = {
       ...values
     };
+    // Apply Validations - Begin
     if (values.fullname.length >= 3) {
-      newValues.flagMessageSuccess = true;
+      newValues.flagValidation = true;
+      if (emailRegex.test(newValues.email)) {
+        newValues.flagValidation = true;
+      } else {
+        newValues.flagValidation = false;
+      }
     } else {
-      newValues.flagMessageError = true;
+      newValues.flagValidation = false;
     }
+    // Apply Validations - End
+    newValues.isSubmit = true;
     setValues(newValues);
   }
 
@@ -46,15 +55,22 @@ const Form = () => {
         <input
           id="email"
           name="email"
-          type="email"
+          type="text"
           value={values.email}
           onChange={handleChange}
           placeholder='Email'
         />
         <button type="submit">Send</button>
       </form>
-      <label hidden={!values.flagMessageSuccess}>Gracias {values.fullname}, te contactaremos cuando antes vía mail</label>
-      <label hidden={!values.flagMessageError}>Por favor, verifique su información nuevamente</label>
+      {values.isSubmit ? (
+        <>
+          <label hidden={!values.flagValidation}>Gracias {values.fullname}, te contactaremos cuando antes vía mail</label>
+          <label hidden={values.flagValidation}>Por favor, verifique su información nuevamente</label>
+        </>
+      ) : (
+        <>
+        </>
+      )}
     </div >
   );
 };
